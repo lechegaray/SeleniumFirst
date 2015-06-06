b6a986e05babe9625e85c50d2f4910d331a74bd7
Luis Echegaray <mrluisechegaray@gmail.com>
2015-06-05 20:46:47 -0700

    setup dependencies

---
+++ SeleniumFirst/Program.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create the reference for the browser
+            IWebDriver driver = new ChromeDriver();
+        }
+    }
+}
